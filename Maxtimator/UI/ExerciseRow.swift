e446fc3ecfe722e1001f4b0a257639f3c91fcbc7
Bear Cahill <bear@brainwashinc.com>
2024-05-18 14:40:18 -0500

    split UI into files

---
+++ Maxtimator/UI/ExerciseRow.swift
@@ -0,0 +1,8 @@
+//
+//  ExerciseRow.swift
+//  Maxtimator
+//
+//  Created by Bear on 5/18/24.
+//
+
+import Foundation
---
+++ Maxtimator/UI/MaxChart.swift
@@ -0,0 +1,8 @@
+//
+//  MaxChart.swift
+//  Maxtimator
+//
+//  Created by Bear on 5/18/24.
+//
+
+import Foundation
